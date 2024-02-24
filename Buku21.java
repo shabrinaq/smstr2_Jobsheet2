38b0d5aff6fd05bf29b084a4a6f78b9a82fb8287
shabrinaq <shabrinaqottrunnada@gmail.com>
2024-02-24 16:46:15 +0700

    first commit

---
+++ Buku21.java
@@ -0,0 +1,34 @@
+/**
+ * Buku21
+ */
+public class Buku21 {
+    String judul, pengarang;
+    int halaman, stok, harga;
+
+    void tampilanInformasi() {
+        System.out.println("Judul: " + judul);
+        System.out.println("Pengarang: " + pengarang);
+        System.out.println("Jumlah halaman: " + halaman);
+        System.out.println("Sisa stok: " + stok);
+        System.out.println("Harga: Rp " + harga);
+    }
+
+    void terjual(int jml) {
+        if (stok > 0) {
+            stok -= jml;
+            if (stok > 0) {
+                stok = 0; //unutk membiarkan stok ke 0 tetap menetap jika stok negatif(tdk baik) 
+            }
+        } else {
+            System.out.println("Mohon Maaf stok buku tidak ada!");
+        }
+    }
+
+    void restock(int jml) {
+        stok += jml;
+    }
+
+    void gantiHarga(int hrg) {
+        harga = hrg;
+    }
+}
---
+++ BukuMain21.java
@@ -0,0 +1,19 @@
+/**
+ * BukuMain21
+ */
+public class BukuMain21 {
+
+    public static void main(String[] args) {
+        Buku21 bk1 = new Buku21(); //instansiasi nuku
+        bk1.judul = "Today Ends Tomorrow Comes"; //untuk menampilkan nilai pada atribut buku
+        bk1.pengarang = "Denanda Pratiwi";
+        bk1.halaman = 198;
+        bk1.stok = 13;
+        bk1.harga = 71000;
+
+        bk1.tampilanInformasi(); //untuk menmapilkan informasi sebelum terjual dan ganti harga
+        bk1.terjual(5); //buku yang terjual(stok)
+        bk1.gantiHarga(60000); //mengubah harga buku
+        bk1.tampilanInformasi(); //menampilkan informasi buku setelah terjual dan ganti harga    
+    }
+}
