58addb90afec0fb2c6e24ec075e8ee44908ce8df
shabrinaq <shabrinaqottrunnada@gmail.com>
2024-02-24 18:53:37 +0700

    change

--- BukuMain21.java
+++ BukuMain21.java
@@ -4,17 +4,17 @@
 public class BukuMain21 {
 
     public static void main(String[] args) {
-        Buku21 bk1 = new Buku21(); //instansiasi nuku
-        bk1.judul = "Today Ends Tomorrow Comes"; //untuk menampilkan nilai pada atribut buku
+        Buku21 bk1 = new Buku21(); 
+        bk1.judul = "Today Ends Tomorrow Comes";
         bk1.pengarang = "Denanda Pratiwi";
         bk1.halaman = 198;
         bk1.stok = 13;
         bk1.harga = 71000;
 
-        bk1.tampilanInformasi(); //untuk menmapilkan informasi sebelum terjual dan ganti harga
-        bk1.terjual(5); //buku yang terjual(stok)
-        bk1.gantiHarga(60000); //mengubah harga buku
-        bk1.tampilanInformasi(); //menampilkan informasi buku setelah terjual dan ganti harga  
+        bk1.tampilanInformasi(); 
+        bk1.terjual(5); 
+        bk1.gantiHarga(60000); 
+        bk1.tampilanInformasi(); 
         
         Buku21 bk2 = new Buku21("Self Reward", "Maheera Ayesha", 160, 29, 59000);
         bk2.terjual(11);
